d2ee6622ff7ea450ded17ddae6e1af9f1bb26b6c
Tyler Drennan <Tyler@Tylers-MBP.attlocal.net>
2016-02-10 00:18:51 -0600

    added buttons and files

--- Bomb/ViewController.swift
+++ Bomb/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    @IBOutlet weak var hideRed: UIButton!
+    @IBOutlet weak var hideBlue: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideTheBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+        redBomb.hidden = false
+    }
+    
+    @IBAction func hideTheRedBomb(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = true
+    }
+    
+    @IBAction func resetView(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = false
+    }
 
 }
 
